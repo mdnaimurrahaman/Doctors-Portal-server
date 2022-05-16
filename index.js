cfe483379e2c7755962bc93cdd919dc1b16e36c0
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-16 18:24:39 +0600

    A new put api has been created for adding admin

--- index.js
+++ index.js
@@ -46,6 +46,27 @@ async function run(){
             res.send(services);
         });
 
+        app.get('/user',verifyJWT, async(req, res)=>{
+          const users = await userCollection.find().toArray();
+          res.send(users)
+
+        })
+
+        // create admin user
+        app.put('/user/admin/:email', verifyJWT, async(req, res)=>{
+          const email = req.params.email;
+          const filter = {email: email};
+          const updateDoc = {
+            $set: {role: 'admin'},
+          };
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.send(result);
+
+        })
+
+
+
+
         app.put('/user/:email', async(req, res)=>{
           const email = req.params.email;
           const user = req.body;
@@ -55,7 +76,7 @@ async function run(){
             $set: user,
           };
           const result = await userCollection.updateOne(filter, updateDoc, options);
-          const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+          const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '7d'});
           res.send({result, token });
 
         })
