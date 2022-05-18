dcb57e2bc5bdf4ba712f6d322954beb2b73acbc9
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-18 18:18:21 +0600

    Added new

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function verifyJWT(req, res, next){
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
     if(err){
+      console.log(err)
       return res.status(403).send({message: 'Forbidden access'})
     }
     req.decoded = decoded;
@@ -39,6 +40,20 @@ async function run(){
         const bookingCollection = client.db('doctors_Portal').collection('bookings');
         const userCollection = client.db('doctors_Portal').collection('users');
         const doctorCollection = client.db('doctors_Portal').collection('doctors');
+
+
+      const verifyAdmin = async (req, res, next) => {
+
+        const requester = req.decoded.email;
+        const requesterAccount = await userCollection.findOne({email: requester});
+          if (requesterAccount.role === 'admin'){
+            next()
+          }
+          else{
+            res.status(403).send({message: 'forbidden'})
+          }
+      }
+
       
         app.get('/service', async(req, res) =>{
             const query = {};
@@ -54,25 +69,14 @@ async function run(){
         })
 
         // create admin user
-        app.put('/user/admin/:email', verifyJWT, async(req, res)=>{
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async(req, res)=>{
           const email = req.params.email;
-
-          const requester = req.decoded.email;
-          const requesterAccount = await userCollection.findOne({email: requester});
-
-          if (requesterAccount.role === 'admin'){
-
           const filter = {email: email};
           const updateDoc = {
             $set: {role: 'admin'},
           };
           const result = await userCollection.updateOne(filter, updateDoc);
           res.send(result);
-          }
-
-          else{
-            res.status(403).send({message: 'forbidden'})
-          }
 
         })
 
@@ -168,7 +172,7 @@ async function run(){
 
 
         // Doctors api creating
-        app.post('/doctor', async(req, res) => {
+        app.post('/doctor',verifyJWT, verifyAdmin, async(req, res) => {
           const doctor = req.body;
           const result = await doctorCollection.insertOne(doctor);
           res.send(result);
