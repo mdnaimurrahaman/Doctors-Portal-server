47c5d98fd9419578e80246691b7fab05f5c07bc3
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-18 14:52:59 +0600

    Post api was created to add doctor's data

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ async function run(){
         const serviceCollection = client.db('doctors_Portal').collection('services');
         const bookingCollection = client.db('doctors_Portal').collection('bookings');
         const userCollection = client.db('doctors_Portal').collection('users');
+        const doctorCollection = client.db('doctors_Portal').collection('doctors');
       
         app.get('/service', async(req, res) =>{
             const query = {};
@@ -163,7 +164,17 @@ async function run(){
           }
           const result = await bookingCollection.insertOne(booking);
           return res.send({success: true, result});
+        });
+
+
+        // Doctors api creating
+        app.post('/doctor', async(req, res) => {
+          const doctor = req.body;
+          const result = await doctorCollection.insertOne(doctor);
+          res.send(result);
         })
+
+
     }
     finally{
 
