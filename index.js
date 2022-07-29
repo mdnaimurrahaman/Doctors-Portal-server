6f49878ba391117811a5c28a4b59eeccf44559bf
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-07-29 20:58:13 +0600

    new function added

--- index.js
+++ index.js
@@ -239,13 +239,6 @@ async function run(){
           const result = await doctorCollection.insertOne(doctor);
           res.send(result);
         })
-        // Doctors delete api 
-        app.delete('/doctor/:email',verifyJWT, verifyAdmin, async(req, res) => {
-          const email = req.params.email;
-          const filter = {email: email};
-          const result = await doctorCollection.deleteOne(filter);
-          res.send(result);
-        })
 
 
     }
@@ -262,3 +255,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Doctors app listening on port ${port}`)
 })
+
